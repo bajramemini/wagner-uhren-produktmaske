45aa9ea408bbf21d105f0f6090285ad9c288116e
Bajram Emini <bajram@eminiarts.ch>
2020-11-30 10:46:20 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  theme: {
+    fontFamily: {
+      sans: [
+        '"Proxima Nova"',
+        '"Helvetica Neue"',
+        "Helvetica",
+        "Arial",
+        "sans-serif"
+      ],
+      serif: ['"Freight Display"', "Georgia", '"sans-serif"']
+    }
+  },
+  variants: {},
+  plugins: []
+};
